7a7d0ab300fb961a75b2c9cf2d0562babae29cbe
Lev Berlinkov <levan90@mail.ru>
2022-02-14 09:09:14 +0200

    update signature

--- app/src/main/java/com/example/heroes/adapters/BindingAdapters.kt
+++ app/src/main/java/com/example/heroes/adapters/BindingAdapters.kt
@@ -6,6 +6,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.heroes.R
+import com.example.heroes.adapters.OneDayExpirationSignature.Companion.versionNumber
 
 @BindingAdapter("imageFromUrl")
 fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
@@ -15,7 +16,7 @@ fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
             .load(imageUrl)
             .placeholder(R.drawable.ic_launcher_background)
             .error(R.drawable.noob_noob)
-            .signature(OneDayExpirationSignature(1))
+            .signature(OneDayExpirationSignature(versionNumber))
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(view)
--- app/src/main/java/com/example/heroes/adapters/OneDayExpirationSignature.kt
+++ app/src/main/java/com/example/heroes/adapters/OneDayExpirationSignature.kt
@@ -7,16 +7,17 @@ import java.util.*
 
 //https://bumptech.github.io/glide/doc/caching.html
 //https://tech.fleka.me/how-to-have-an-expired-cache-for-glide-image-library-e69d9b54ef40
-class OneDayExpirationSignature(private var currentVersion: Int) : Key {
+class OneDayExpirationSignature(var currentVersion: Int) : Key {
 
     companion object {
-        var versionNumber: Int = Calendar.getInstance().get(Calendar.DAY_OF_YEAR) * 100 +
+        //unique id of day
+        val versionNumber: Int = Calendar.getInstance().get(Calendar.DAY_OF_YEAR) * 100 +
                 Calendar.getInstance().get(Calendar.YEAR)
     }
-
+    //if current version (id of today) not equals of id of day created, then update key and download again
     override fun equals(other: Any?): Boolean {
         if (other is OneDayExpirationSignature)
-            return other == currentVersion
+            return other.currentVersion == currentVersion
         return false
     }
 
@@ -25,6 +26,6 @@ class OneDayExpirationSignature(private var currentVersion: Int) : Key {
     }
 
     override fun updateDiskCacheKey(messageDigest: MessageDigest) {
-        messageDigest.update(ByteBuffer.allocate(Integer.SIZE).putInt(versionNumber).array())
+        messageDigest.update(ByteBuffer.allocate(Integer.SIZE).putInt(currentVersion).array())
     }
 }
